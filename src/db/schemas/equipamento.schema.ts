85a12a91193d597d60fa9f6a0d47fc4c3b68eb55
Somma <gustavosferracini@Proton.me>
2026-08-11 21:31:13 -0300

    trocando linguagem de programacao
    js/ts > go

--- src/db/schemas/equipamento.schema.ts
+++
@@ -1,9 +0,0 @@
-import { sqliteTable, text, integer } from 'drizzle-orm/sqlite-core'
-
-export const equipamentos = sqliteTable('equipamentos', {
-  id: integer('id').primaryKey(),
-  marca: text('marca').notNull(),
-  modelo: text('modelo').notNull(),
-  status: text('status').notNull(),
-  manutencao: integer('manutencao', { mode: 'boolean' }),
-})
